8cc08d42011893d53b2e41c0a6276784e26dbe28
meyta2020 <tmeysner@gmail.com>
2020-11-03 22:20:13 -0600

    add

--- test/smoke/mainFunctionality.js
+++ test/smoke/mainFunctionality.js
@@ -1,4 +1,5 @@
 const sel = require('../../data/selectors.json');
+const data = require('../../data/testData.json');
 
 describe('Checking the main functionality', function () {
 
@@ -6,13 +7,13 @@ describe('Checking the main functionality', function () {
 
         it('TC-021 Create button is clickable after 1-4 are field in', function () {
             browser.url('');
-            const inputName = $(sel.name).setValue("LadyBug");
+            const inputName = $(sel.name).setValue(data.name);
             const inputGender = $$(sel.gender)[1].click();
-            const inputAge = $(sel.age).setValue(123);
+            const inputAge = $(sel.age).setValue(data.age);
             const click = $(sel.storyClick).click();
             const inputStory = $$(sel.storyType)[6].click();
             const create = $(sel.submitButton).isEnabled();
-            browser.pause(3000);
+            browser.pause(1000);
             expect(create).toEqual(true);
         });
     });
