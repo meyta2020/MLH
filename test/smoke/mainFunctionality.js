872d5f74b4bd01c887dfacd3bbdd5b38264c553f
meyta2020 <tmeysner@gmail.com>
2020-11-04 20:48:24 -0600

    add TC

--- test/smoke/mainFunctionality.js
+++ test/smoke/mainFunctionality.js
@@ -6,20 +6,7 @@ const inputValues4andClick = require('../../helpers/inputValues4andClick');
 describe('Checking the main functionality', function () {
 
     describe('Happy path', function () {
-/*
-        it('TC-021 Create button is clickable after 1-4 are field in', function () {
-            browser.url('');
-            const inputName = $(sel.name).setValue(data.name);
-            const inputGender = $$(sel.gender)[data.gender.she].click();
-            browser.pause(2000);
-            const inputAge = $(sel.age).setValue(data.age);
-            const click = $(sel.storyClick).click();
-            const inputStory = $$(sel.storyType)[data.storyType].click();
-            const create = $(sel.submitButton).isEnabled();
-            browser.pause(1000);
-            expect(create).toEqual(true);
-        });
-*/
+
         it('TC-022 Create button is clickable after 1-4 are field in', function () {
             browser.url('');
             inputValues4(data.name, data.gender.she, data.age, data.storyType);
@@ -30,9 +17,27 @@ describe('Checking the main functionality', function () {
     });
 
     describe('Other patch', function () {
-        it('TC-023 gender he is working', function () {
+        it('TC-023 gender she is working', function () {
             browser.url('');
             inputValues4andClick(data.name, data.gender.she, data.age, data.storyType);
+            browser.pause(1000);
+            const btn  = $(sel.tryAgain).isDisplayed()
+            expect(btn).toEqual(true);
+            browser.pause(1000);
+        });
+
+        it('TC-024 gender he is working', function () {
+            browser.url('');
+            inputValues4andClick(data.name, data.gender.he, data.age, data.storyType);
+            browser.pause(1000);
+            const btn  = $(sel.tryAgain).isDisplayed()
+            expect(btn).toEqual(true);
+            browser.pause(1000);
+        });
+        it('TC-025 gender it is working', function () {
+            browser.url('');
+            inputValues4andClick(data.name, data.gender.it, data.age, data.storyType);
+            browser.pause(1000);
             const btn  = $(sel.tryAgain).isDisplayed()
             expect(btn).toEqual(true);
             browser.pause(1000);
